c0b083c382d0b4d06cd08c940c6ec967d50d97d4
Алексей <alesha1986@bk.ru>
2023-04-04 19:05:09 +0300

    Classes and objects2

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,31 @@
 public class Main {
 
     public static void main(String[] args) {
-        Author pushkin = new Author ("Александр", "Пушкин");
+        Author pushkin = new Author("Александр", "Пушкин");
         Author blok = new Author("Александр", "Блок");
 
         Book ruslanLudmila = new Book("Руслан и Людмила", pushkin, 1820);
+        Book ruslanLudmila2 = new Book("Руслан и Людмила", pushkin, 1821);
+
         Book stranger = new Book("Незнакомка", blok, 1906);
 
-        System.out.println("Заголовок книги: " + ruslanLudmila.getTitle());
-        System.out.println("Год издания книги: " + ruslanLudmila.getPublishingYear() );
+        // System.out.println("Заголовок книги: " + ruslanLudmila.getTitle());
+        // System.out.println("Год издания книги: " + ruslanLudmila.getPublishingYear() );
 
         ruslanLudmila.setPublishingYear(1821);
 
-        System.out.println("Год переиздания книги: " + ruslanLudmila.getPublishingYear());
+        // System.out.println("Год переиздания книги: " + ruslanLudmila.getPublishingYear());
 
-        System.out.println("Заголовок книги: " + stranger.getTitle());
-        System.out.println("Год издания книги: " + stranger.getPublishingYear() );
+        // System.out.println("Заголовок книги: " + stranger.getTitle());
+        // System.out.println("Год издания книги: " + stranger.getPublishingYear() );
+
+        System.out.println(pushkin.toString());
+        System.out.println(ruslanLudmila.toString());
+
+        System.out.println(blok.hashCode());
+        System.out.println(stranger.hashCode());
+
+        System.out.println(ruslanLudmila.equals(stranger));
+        System.out.println(ruslanLudmila.equals(ruslanLudmila2));
     }
 }
